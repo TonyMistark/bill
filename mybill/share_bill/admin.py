fbb2525a6ac5e7863c73e7ec9d6425a5fed89c6b
ice <tony_mistark@163.com>
2017-02-19 21:22:11 +0800

    init project

--- mybill/share_bill/admin.py
+++ mybill/share_bill/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bill
 
-# Register your models here.
+
+class BillAdmin(admin.ModelAdmin):
+    list_prr_page = 20
+    list_display = ("title", "ware", "cose")
+    
+admin.site.register(Bill, BillAdmin)
