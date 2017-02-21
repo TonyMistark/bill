0684ca6648946483b57fc4a6452748ea928607e3
ice <tony_mistark@163.com>
2017-02-21 22:52:50 +0800

    fix

--- mybill/settings.py
+++ mybill/settings.py
@@ -97,11 +97,12 @@ DATABASES = {
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bill',                         # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bill',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',                 # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
--- mybill/urls.py
+++ mybill/urls.py
@@ -13,23 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-
-import django
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
+from django.contrib import admin
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 import xadmin
 xadmin.ROOT_PATH_NAME = 'xadmin'
-settings.XADMIN_EXCLUDE_PLUGINS = ['bookmark']
-
+xadmin.EXPORT_MAX = 500
+xadmin.DEFAULT_RELFIELD_STYLE = {'fk': 'fk_raw', 'm2m': 'm2m_raw'}
+settings.XADMIN_EXCLUDE_PLUGINS = ('bookmark', 'topnav', 'themes', 'language', 'refresh', 'sortable', 'chart')
 xadmin.autodiscover()
-
-# from xadmin.plugins import xversion
-# xversion.register_models()
-
-from django.contrib import admin
-admin.autodiscover()
 
 
 urlpatterns = [
